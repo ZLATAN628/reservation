b6bb8bace11c59530199b0029d7c14de6c8df180
ZLATAN628 <ycx540188804@163.com>
2023-04-16 16:18:42 +0800

    support reserve function

--- abi/src/pb/reservation.rs
+++ abi/src/pb/reservation.rs
@@ -78,7 +78,7 @@ pub struct GetResponse {
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct QueryRequest {
+pub struct ReservationQuery {
     #[prost(string, tag = "1")]
     pub resource_id: ::prost::alloc::string::String,
     #[prost(string, tag = "2")]
@@ -92,6 +92,12 @@ pub struct QueryRequest {
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
+pub struct QueryRequest {
+    #[prost(message, optional, tag = "1")]
+    pub query: ::core::option::Option<ReservationQuery>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ListenRequest {}
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -102,7 +108,9 @@ pub struct ListenResponse {
     pub reservation: ::core::option::Option<Reservation>,
 }
 /// reservation status for a given time period
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[derive(
+    sqlx::Type, Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration,
+)]
 #[repr(i32)]
 pub enum ReservationStatus {
     Unknown = 0,
--- reservation/src/lib.rs
+++ reservation/src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use async_trait::async_trait;
+
+mod error;
+mod manager;
+
+pub type ReservationId = String;
+pub type ResourceId = String;
+pub type UserId = String;
+
+#[derive(Debug)]
+pub struct ReservationManager {
+    pool: sqlx::PgPool,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[async_trait]
+pub trait Rsvp {
+    /// make a resvervation
+    async fn reserve(
+        &self,
+        mut rsvp: abi::Reservation,
+    ) -> Result<abi::Reservation, error::ReservationError>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // /// change reservation status (if current status is pending, change it to confirmed)
+    // async fn change_status(
+    //     &self,
+    //     id: ReservationId,
+    // ) -> Result<abi::Reservation, error::ReservationError>;
+
+    // /// update note
+    // async fn update_note(
+    //     &self,
+    //     id: ReservationId,
+    //     note: String,
+    // ) -> Result<abi::Reservation, error::ReservationError>;
+
+    // /// delete reservation
+    // async fn delete_reservation(&self, id: ReservationId) -> Result<(), error::ReservationError>;
+
+    // /// get reservation by id
+    // async fn get_reservation(
+    //     &self,
+    //     id: ReservationId,
+    // ) -> Result<abi::Reservation, error::ReservationError>;
+
+    // /// query reservation
+    // async fn query(
+    //     &self,
+    //     query: abi::ReservationQuery,
+    // ) -> Result<Vec<abi::Reservation>, error::ReservationError>;
 }
